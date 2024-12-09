467765f3772ac072a501f153e645f1aaa8e9f0ba
rakesh <rakesh@jangid.co.in>
2024-12-09 14:29:35 +0530

    bug fixed

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -3,7 +3,7 @@
 </script>
 <template>
     <AppHeader />
-
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-12">
     <h2 class="text-2xl p-2 px-8 border-b-2">Contact Info:</h2>
     <p class="text-lg font-bold px-8">Kothari Mahila mahavidyalaya</p>
     <p class="text-lg px-8">Sankhantal,</p>
